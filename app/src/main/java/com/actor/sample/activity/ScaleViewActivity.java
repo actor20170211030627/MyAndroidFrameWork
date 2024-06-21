eeaaa64d9210d93753c1889f1f2301be272806df
actor20170211030627 <1455198886@qq.com>
2024-06-21 18:52:45 +0800

    add ZoomView

---
+++ app/src/main/java/com/actor/sample/activity/ScaleViewActivity.java
@@ -0,0 +1,20 @@
+package com.actor.sample.activity;
+
+import android.os.Bundle;
+
+import com.actor.sample.databinding.ActivityScaleViewBinding;
+
+/**
+ * description: View缩放
+ * company    :
+ * @author    : ldf
+ * date       : 2024/6/20 on 17:27
+ */
+public class ScaleViewActivity extends BaseActivity<ActivityScaleViewBinding> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("View缩放");
+    }
+}
