2d176b1375c03cb287be7ffe0fe318a86556ca70
actor20170211030627 <1455198886@qq.com>
2024-07-16 23:21:05 +0800

    add MatchParentImageView

---
+++ app/src/main/java/com/actor/sample/activity/MatchParentImageViewTestActivity.java
@@ -0,0 +1,20 @@
+package com.actor.sample.activity;
+
+import android.os.Bundle;
+
+import com.actor.sample.databinding.ActivityMatchParentImageViewTestBinding;
+
+/**
+ * description: 描述
+ * company    :
+ * @author    : ldf
+ * date       : 2024/7/16 on 20:50
+ */
+public class MatchParentImageViewTestActivity extends BaseActivity<ActivityMatchParentImageViewTestBinding> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("MatchParentImageView");
+    }
+}
