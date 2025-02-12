69da147553d5597c1ec6d65385235d4ff1943506
actor20170211030627 <1455198886@qq.com>
2025-02-12 15:10:33 +0800

    `

--- app/src/main/java/com/actor/sample/activity/WheelViewTestActivity.java
+++ app/src/main/java/com/actor/sample/activity/WheelViewTestActivity.java
@@ -28,25 +28,46 @@ public class WheelViewTestActivity extends BaseActivity<ActivityWheelViewTestBin
             mOptionsItems.add("item " + i);
         }
 
-        MyWheelViewTestAdapter mAdapter0 = new MyWheelViewTestAdapter(R.layout.item_wheel_view, 3, false, true);
-        MyWheelViewTestAdapter mAdapter1 = new MyWheelViewTestAdapter(R.layout.item_wheel_view, 3, true, true);
-        MyWheelViewTestAdapter mAdapter2 = new MyWheelViewTestAdapter(R.layout.item_wheel_view, 3, false, true);
-        MyWheelViewTestAdapter mAdapter3 = new MyWheelViewTestAdapter(R.layout.item_wheel_view, 3, true, true);
+        //竖着滑动
+        MyWheelViewTestAdapter mAdapter0 = new MyWheelViewTestAdapter(R.layout.item_wheel_view_vertical, 3, false, true);
+        MyWheelViewTestAdapter mAdapter1 = new MyWheelViewTestAdapter(R.layout.item_wheel_view_vertical, 3, true, true);
+        MyWheelViewTestAdapter mAdapter2 = new MyWheelViewTestAdapter(R.layout.item_wheel_view_vertical, 3, false, true);
+        MyWheelViewTestAdapter mAdapter3 = new MyWheelViewTestAdapter(R.layout.item_wheel_view_vertical, 3, true, true);
+
+        //横着滑动
+        MyWheelViewTestAdapter mAdapter4 = new MyWheelViewTestAdapter(R.layout.item_wheel_view_horizontal, 3, false, true);
+        MyWheelViewTestAdapter mAdapter5 = new MyWheelViewTestAdapter(R.layout.item_wheel_view_horizontal, 3, true, true);
+        MyWheelViewTestAdapter mAdapter6 = new MyWheelViewTestAdapter(R.layout.item_wheel_view_horizontal, 3, false, true);
+        MyWheelViewTestAdapter mAdapter7 = new MyWheelViewTestAdapter(R.layout.item_wheel_view_horizontal, 3, true, true);
+
         //设置是否能打印日志
         mAdapter0.setLoggable(true);
-        mAdapter1.setLoggable(false);
+        mAdapter1.setLoggable(true);
         mAdapter2.setLoggable(true);
-        mAdapter3.setLoggable(true);
+        mAdapter3.setLoggable(false);
+        mAdapter4.setLoggable(false);
+        mAdapter5.setLoggable(false);
+        mAdapter6.setLoggable(false);
+        mAdapter7.setLoggable(false);
 
+        //1.先设置Adapter
         viewBinding.recyclerView0.setAdapter(mAdapter0);
         viewBinding.recyclerView1.setAdapter(mAdapter1);
         viewBinding.recyclerView2.setAdapter(mAdapter2);
         viewBinding.recyclerView3.setAdapter(mAdapter3);
-        //设置数据
+        viewBinding.recyclerView4.setAdapter(mAdapter4);
+        viewBinding.recyclerView5.setAdapter(mAdapter5);
+        viewBinding.recyclerView6.setAdapter(mAdapter6);
+        viewBinding.recyclerView7.setAdapter(mAdapter7);
+        //2.然后获取到数据后, 再设置数据
         mAdapter0.setList(mOptionsItems);
         mAdapter1.setList(mOptionsItems);
         mAdapter2.setList(mOptionsItems);
         mAdapter3.setList(mOptionsItems);
+        mAdapter4.setList(mOptionsItems);
+        mAdapter5.setList(mOptionsItems);
+        mAdapter6.setList(mOptionsItems);
+        mAdapter7.setList(mOptionsItems);
 
         //获取选中item
         viewBinding.btnGetItem.setOnClickListener(v -> {
@@ -54,7 +75,12 @@ public class WheelViewTestActivity extends BaseActivity<ActivityWheelViewTestBin
             String selectedItem1 = mAdapter1.getSelectedItem();
             String selectedItem2 = mAdapter2.getSelectedItem();
             String selectedItem3 = mAdapter3.getSelectedItem();
+            String selectedItem4 = mAdapter4.getSelectedItem();
+            String selectedItem5 = mAdapter5.getSelectedItem();
+            String selectedItem6 = mAdapter6.getSelectedItem();
+            String selectedItem7 = mAdapter7.getSelectedItem();
             LogUtils.errorFormat("selectedItem0=%s, selectedItem1=%s, selectedItem2=%s, selectedItem3=%s", selectedItem0, selectedItem1, selectedItem2, selectedItem3);
+            LogUtils.errorFormat("selectedItem4=%s, selectedItem5=%s, selectedItem6=%s, selectedItem7=%s", selectedItem4, selectedItem5, selectedItem6, selectedItem7);
         });
     }
 }
